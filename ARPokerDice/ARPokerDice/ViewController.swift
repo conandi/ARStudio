eadf5c8f5718a0b1006c21bd991257ef50b0d597
DI, JIAJIA <zl142f@att.com>
2018-09-10 18:07:49 -0700

    code changes

--- ARPokerDice/ARPokerDice/ViewController.swift
+++ ARPokerDice/ARPokerDice/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 import SceneKit
 import ARKit
 
+enum GameState: Int16 {
+    case detectSurface
+    case pointToSurface
+    case swipeToPlay
+}
+
 class ViewController: UIViewController {
 
     // MARK: -Properties
@@ -23,6 +29,11 @@ class ViewController: UIViewController {
                                     SCNVector3(0.05, 0.00, 0.0),
                                     SCNVector3(-0.05, 0.05, 0.02),
                                     SCNVector3(0.05, 0.05, 0.02)]
+    var focusNode: SCNNode!
+    var gameState: GameState = .detectSurface
+    var statusMessage: String = ""
+    var focusPoint: CGPoint!
+    
     // MARK: -Outlets
     
     @IBOutlet var sceneView: ARSCNView!
@@ -58,13 +69,33 @@ class ViewController: UIViewController {
         return true
     }
     
+    func updateStatus() {
+        switch gameState {
+        case .detectSurface:
+            statusMessage = "Scan entire table surface... \nHit START when ready!"
+        case .pointToSurface:
+            statusMessage = "Point ar designated surface fist!"
+        case .swipeToPlay:
+            statusMessage = "Swipe UP throw!\nTap on dice to collect it again."
+        }
+        self.statusLabel.text = trackingStatus != "" ? "\(trackingStatus)" : "\(statusMessage)"
+    }
+    
+    @objc func orientationChanged() {
+        focusPoint = CGPoint(x: view.center.x,
+                             y: view.center.y + view.center.y*0.25)
+    }
+    
     // MARK: - Initalization
     
     func initSceneView() {
         sceneView.delegate = self
         sceneView.showsStatistics = true
-        sceneView.debugOptions = [.showBoundingBoxes, .showWireframe, ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
-        
+//        sceneView.debugOptions = [.showBoundingBoxes, .showWireframe, ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        focusPoint = CGPoint(x: view.center.x, y: view.center.y + view.center.y*0.25)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(ViewController.orientationChanged),
+                                               name: Notification.Name.UIDeviceOrientationDidChange, object: nil)
     }
     
     func initScene() {
@@ -84,6 +115,7 @@ class ViewController: UIViewController {
         let config  = ARWorldTrackingConfiguration()
         config.worldAlignment = .gravity
         config.providesAudioData = false
+        config.planeDetection = .horizontal
         sceneView.session.run(config)
     }
     
@@ -92,9 +124,17 @@ class ViewController: UIViewController {
         
     }
     @IBAction func styleButtonPressed(_ sender: Any) {
-        
+        diceStyle = diceStyle >= 4 ? 0 : diceStyle + 1
     }
     @IBAction func resetButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func swipeUPGestureHandler(_ sender: Any) {
+        guard let frame  = self.sceneView.session.currentFrame else { return }
+        
+        for count in 0 ..< diceCount {
+            throwDiceNode(transform: SCNMatrix4(frame.camera.transform), offset: diceOffset[count])
+        }
     }
     
     // MARK: - Load Models
@@ -103,6 +143,9 @@ class ViewController: UIViewController {
         for count in 0 ..< 5 {
             diceNodes.append(diceScene.rootNode.childNode(withName: "dice\(count)", recursively: false)!)
         }
+        let focuScene = SCNScene(named: "PokerDice.scnassets/Models/FocusScene.scn")!
+        focusNode = focuScene.rootNode.childNode(withName: "focus", recursively: false)!
+        sceneView.scene.rootNode.addChildNode(focusNode)
     }
     
     // MARK: - Helper Functions
@@ -115,7 +158,41 @@ class ViewController: UIViewController {
         diceNode.name = "dice"
         diceNode.position = position
         sceneView.scene.rootNode.addChildNode(diceNode)
-        diceCount -= 1
+//        diceCount -= 1
+    }
+    
+    func createARPlaneNode(planeAnchor: ARPlaneAnchor, color: UIColor) -> SCNNode {
+        let planeGeometry = SCNPlane(width: CGFloat(planeAnchor.extent.x),
+                                     height: CGFloat(planeAnchor.extent.z))
+        let planeMaterial = SCNMaterial()
+        planeMaterial.diffuse.contents = "PokerDice.scnassets/Textures/Surface_DIFFUSE.png"
+        planeGeometry.materials = [planeMaterial]
+        let planeNode = SCNNode(geometry: planeGeometry)
+        planeNode.position = SCNVector3Make(planeAnchor.center.x, 0, planeAnchor.center.z)
+        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+        return planeNode
+    }
+    
+    func updateARPlaneNode(planeNode: SCNNode, planeAchor: ARPlaneAnchor) {
+        let planeGeometry = planeNode.geometry as! SCNPlane
+        planeGeometry.width = CGFloat(planeAchor.extent.x)
+        planeGeometry.height = CGFloat(planeAchor.extent.z)
+        planeNode.position = SCNVector3Make(planeAchor.center.x, 0, planeAchor.center.z)
+    }
+    
+    func updateFocusNode() {
+        let results = self.sceneView.hitTest(self.focusPoint, types: [.existingPlaneUsingExtent])
+        if results.count == 1 {
+            if let match = results.first {
+                let t = match.worldTransform
+                self.focusNode.position = SCNVector3(x: t.columns.3.x,
+                                                     y: t.columns.3.y,
+                                                     z: t.columns.3.z)
+                self.gameState = .swipeToPlay
+            }
+        } else {
+            self.gameState = .pointToSurface
+        }
     }
 }
 
@@ -124,7 +201,8 @@ extension ViewController: ARSCNViewDelegate {
     // MARK: -SceneKit Management
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
         DispatchQueue.main.async {
-            self.statusLabel.text = self.trackingStatus
+            self.updateStatus()
+            self.updateFocusNode()
         }
     }
     
@@ -164,4 +242,20 @@ extension ViewController: ARSCNViewDelegate {
     }
     
     // MARK: - Plane Mangement
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        DispatchQueue.main.async {
+            let planeNode = self.createARPlaneNode(planeAnchor: planeAnchor,
+                                                   color: UIColor.yellow.withAlphaComponent(0.5))
+            node.addChildNode(planeNode)
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        DispatchQueue.main.async {
+            self.updateARPlaneNode(planeNode: node.childNodes[0], planeAchor: planeAnchor)
+        }
+    }
 }
